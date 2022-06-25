0a6eca31307682c52a95fb616081546aa9cca411
EMPRATOR <78980892+XalaEMPRATOR@users.noreply.github.com>
2022-06-25 17:43:30 +0300

    Update emprator.js

--- emprator.js
+++ emprator.js
@@ -27,6 +27,7 @@ const data = new Map();
 client.on("message", SAEWAN => {
   if (SAEWAN.content.startsWith("ریکلام دەکەن")) {
     SAEWAN.author.send("https://discord.gg/jKjF3J2sJd");
+SAEWAN.reply("");
   }
 });
 ///////
